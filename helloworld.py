bae4885eb3975b8ab75e1f3e970024fa5e50f29a
ACollins00 <113385886+ACollins00@users.noreply.github.com>
2022-10-06 09:49:35 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ print("Hello World")
 adopting_dog = ["shelter", "breeder", "store"]
 print(adopting_dog[2])
 dog_breeds = {"Yorkie", "Bulldog", "Beagle", "Poodle", "1", "2"}
-print("dog breeds", "[1]")
+print("dog_breeds", "[1]")
 my_tuple = (1, 2, 3, 4, 5)
 print(my_tuple[2])
 
